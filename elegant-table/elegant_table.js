fbd70092ad4707ae5b37de34ec0c67f6301ffdeb
Joel Garcia Jr <joel.garciajr84@gmail.com>
2017-02-23 16:02:45 -0300

    Added bootstrap

--- elegant-table/elegant_table.js
+++ elegant-table/elegant_table.js
@@ -13,16 +13,32 @@ var data = [
 ];
 
 var AirBusPlanes = React.createClass({
+    getInitialState: function() {
+        return {
+            data: this.props.initialData
+        };
+    },
     displayName: "AirBusPlanes",
     render:function(){
         return(
-            React.DOM.table(null,
-                React.DOM.thead(null,
+            React.DOM.table({className:"table table-striped table-inverse"},
+                React.DOM.thead({className:"thead-inverse"},
                     React.DOM.tr(null,
                         this.props.headers.map(function(title, idx){
-                            return React.DOM.th({key:idx},title)
+                            return React.DOM.th({key:idx},title);
                         })
                     )
+                ),
+                React.DOM.tbody(null,
+                    this.state.data.map(function(row,idx){
+                        return(
+                            React.DOM.tr({key:idx},
+                                row.map(function(cell, idx){
+                                    return React.DOM.td({key:idx}, cell);
+                                })
+                            )
+                        )
+                    })
                 )
             )
         );
